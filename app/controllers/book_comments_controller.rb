00095586300d7a1474f08769c9edd84dcb7df0ce
EC2 Default User <ec2-user@ip-172-31-6-204.ap-northeast-1.compute.internal>
2023-04-30 04:04:01 +0000

    レビュー_2回目

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,30 @@
 class BookCommentsController < ApplicationController
   def create
+    # @book = Book.find(params[:book_id])
+    # @comment = current_user.book_comments.new(book_comment_params)
+    # @comment.book_id = @book.id
+    # @comment.save
+    # # redirect_to book_path(book.id)
+    # @book_comments = Book.find(params[:book_id]).book_comments
+    
     book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = book.id
-    @comment.save
-    redirect_to book_path(book.id)
+  comment = current_user.book_comments.new(book_comment_params)
+  comment.book_id = book.id
+  comment.save
+  @book=Book.find(params[:book_id])
+  @book_comment = BookComment.new
+  # if @book_comment.save
+  #   respond_to do |format|
+  #     format.js
+  #   end
+  #   end
   end
 
   def destroy
-   book = Book.find(params[:book_id])
+   @book = Book.find(params[:book_id])
     @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @comment.destroy
-    redirect_to book_path(book.id)
+    # redirect_to book_path(book.id)
   end
 
   private
